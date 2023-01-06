1310a2fd81e77b7d05d51b4a34fc9369742c37c9
botormi1 <75608207+botormi1@users.noreply.github.com>
2023-01-06 15:42:30 +0100

    Feature xyz (#2)
    * adding a welcome message to 'main.go'
    
    * adding integer to 'main.go'
    
    * Update main.go
    
    * adding commit from VS Code!

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 func main() {
+  fmt.Println("Hello, Go!")
+  fmt.Println("Welcome to my program.")
+  var a int = 100
+  fmt.Println("{}", a)
   fmt.Println("max(1, 2) = {}", max(1, 2))
 }
 
