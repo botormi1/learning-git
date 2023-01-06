9de87b558b6d162f5a3ddd11cb1716e223b8f3f4
botormi1 <75608207+botormi1@users.noreply.github.com>
2023-01-06 14:15:54 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 func main() {
   fmt.Println("Hello, Go!")
   fmt.Println("Welcome to my program.")
+  var a int = 100
+  fmt.Println("{}", a)
   fmt.Println("max(1, 2) = {}", max(1, 2))
 }
 
 func max(a, b int) int {
   return a > b ? a : b
-    var a int = 100
-    fmt.Println("Hello, Go!")
-    fmt.Println("{}", a)
 }
